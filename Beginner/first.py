bce26096c0c62a085ff2948ffedd5593d2e80eec
StariyLis <avkutch@gmail.com>
2019-05-18 13:08:26 +0400

    18.05.2019 13:00

--- Beginner/first.py
+++ Beginner/first.py
@@ -1,12 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
-from collections import Counter
+import pickle
+import os.path
 
 
 class Stoloto():
     def __init__(self, url):
         self.url = url
-        print('Класс ' + self.url.split('/')[3] + ' создан')
+        self.path = './db_' + self.url.split('/')[3] + '.dat'
+        self.draw_ball= {}
 
 
     def get_html(self):
@@ -15,10 +17,7 @@ class Stoloto():
 
 
     def get_date(self):
-        draw_ball= {}
-        draw = []
-        date = []
-        time = []
+
         ball = []
         soup = BeautifulSoup(self.get_html(), 'lxml')
         month = soup.findAll('div', class_='month')
@@ -31,15 +30,20 @@ class Stoloto():
                 try:
                     b = i.find('div', class_='container cleared').find('span', class_='zone').find_all('b')
                     ball = [int(z.text) for z in b]
-
                 except Exception:
                     print('Ahhhhhh')
-                draw_ball[draw] = ball
+                self.draw_ball[draw] = ball
 
-        return draw_ball
+        if not os.path.exists(self.path):
+            with open(self.path, 'wb') as fw:
+                pickle.dump(self.draw_ball, fw)
+        else:
+            if os.path.getsize(self.path) != 0:
+                with open(self.path, 'rb') as fr:
+                    read_data = pickle.load(fr)
+
+        return self.draw_ball
 
 
-lotto_5x36plus = Stoloto('https://www.stoloto.ru/5x36plus/archive')
-lotto_6x45 = Stoloto('https://www.stoloto.ru/6x45/archive')
-
-print(lotto_5x36plus.get_date())
+top3 = Stoloto('https://www.stoloto.ru/top3/archive')
+print(top3.get_date())
